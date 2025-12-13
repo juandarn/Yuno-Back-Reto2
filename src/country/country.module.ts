e01246cad40b9ffa2645866502ad0223d49315d5
Feder1414 <f.bogota@uniandes.edu.co>
2025-12-13 14:41:44 -0500

    Solucionado repositorios

--- src/country/country.module.ts
+++ src/country/country.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CountriesService } from './country.service';
 import { CountriesController } from './country.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Country } from './entities/country.entity';
 @Module({
+  imports: [TypeOrmModule.forFeature([Country])],
   controllers: [CountriesController],
   providers: [CountriesService],
   exports: [CountriesService],
--- src/payment-method/payment-method.module.ts
+++ src/payment-method/payment-method.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PaymentMethodsService } from './payment-method.service';
 import { PaymentMethodsController } from './payment-method.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PaymentMethod } from './entities/payment-method.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([PaymentMethod])],
   controllers: [PaymentMethodsController],
   providers: [PaymentMethodsService],
   exports: [PaymentMethodsService],
